dfa21fd372c6a2c9cfa55df4bd9baa82dc93dfab
JulieSemenova <julie.loggi@gmail.com>
2021-04-05 10:12:01 +0300

    add typescript and scripts for check-types, update babel build

--- src/typescript-example.ts
+++ src/typescript-example.ts
@@ -1,14 +1,22 @@
-function add(a, b) {
+function add(a: number, b: number): number {
   return a + b;
 }
 
-function getFullName(user) {
+interface User {
+  name: {
+    first: string;
+    middle: string;
+    last: string;
+  };
+}
+
+function getFullName(user: User): string {
   const {
     name: {first, middle, last},
   } = user;
   return [first, middle, last].filter(Boolean).join('');
 }
 
-add(1, 'two');
+add(1, 2);
 
-getFullName({name: {first: 'Joe', m1ddle: 'Bud', last: 'Matthews'}});
+getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}});
